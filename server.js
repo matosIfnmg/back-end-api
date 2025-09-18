f6384873282bfea0b0a972549aeceb15046f5c96
Vinicius Araújo Matos <vam8@aluno.ifnmg.edu.br>
2025-09-18 17:03:18 +0000

    V2 node

--- server.js
+++ server.js
@@ -2,28 +2,45 @@
 // 1. Importação de Pacotes
 // ######
 import express from "express"; // Importa o framework Express
+import pkg from "pg";
+import dotenv from "dotenv";
 
 // ######
 // 2. Configurações do Servidor
 // ######
-const app = express();         // Cria uma instância do Express para gerenciar o servidor
-const port = 3000;             // Define a porta que o servidor vai "escutar"
+dotenv.config();         // Carrega e processa o arquivo .env
+const { Pool } = pkg;    // Utiliza a Classe Pool do Postgres
+
+const app = express();
+const port = 3000;
+
+const db = new Pool({  
+  connectionString: process.env.URL_BD,
+});
 
 // ######
 // 3. Definição de Rotas (Endpoints)
 // ######
 
 // Rota GET para o caminho raiz ("/")
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   // req (request): Contém os dados da requisição que chegou.
   // res (response): É o objeto que usamos para enviar uma resposta de volta.
 
-  console.log("Rota GET / foi solicitada com sucesso!"); // Mensagem no terminal do servidor
+  let dbStatus = "ok";
+  try {
+    await db.query("SELECT 1");
+  } catch (e) {
+    dbStatus = e.message;
+  }
+  
+  console.log("Rota GET / foi solicitada com sucesso!");
 
   // Envia uma resposta em formato JSON com seus dados
   res.json({
-    descricao: "API de exemplo para atividade #13",
-    autor: "Vinicius araujo matos",
+    descricao: "API de exemplo para a atividade #16",
+    autor: "Vinicius Araujo Matos",
+    statusBD: dbStatus,
   });
 });
 
